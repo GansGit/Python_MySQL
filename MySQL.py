410ae47dc1ca6377064034d78b3c708bf415bd6c
GansGit <fenukraine@gmail.com>
2018-03-02 08:45:56 +0200

    Add files via upload

---
+++ MySQL.py
@@ -0,0 +1,14 @@
+import bottle
+from bottle import route, run
+import MySQLdb as myDB
+
+conn = myDB.connect('localhost', 'root', 'pass', 'test')
+cur = conn.cursor()
+cur.execute ("Select version()")
+result = cur.fetchone()
+print "DataBase version : %s" %result
+########################
+cur.execute ("Select * from books")
+rows - cur.fetchall()
+for row in rows:
+    print row
